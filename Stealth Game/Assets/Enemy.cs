33c2a1c73fe74011d4ff9dd706d5cdd9c4b5a8ab
Darcy Phipps <diphipps@ucsc.edu>
2021-12-04 23:25:01 -0800

    Added proof of concept barrier

--- Stealth Game/Assets/Enemy.cs
+++ Stealth Game/Assets/Enemy.cs
@@ -16,7 +16,8 @@ public class Enemy : MonoBehaviour
     	//We're doing it from the player's height
     	Vector3 toPlayer = this.player.transform.position - new Vector3(this.transform.position.x, this.player.transform.position.y, this.transform.position.z);
     	//Figure out the angle, to see if it's in the vision cone
-    	if(Vector3.Angle(toPlayer, this.transform.forward) < 60){
+        //Debug.Log(Vector3.Angle(toPlayer, this.transform.forward));
+    	if(Vector3.Angle(toPlayer, this.transform.forward) < 40){
     		Debug.Log("Potential spot");
     		//We're going to try raycasting now to the left, middle, and right bits of the player
     		//If any of them don't hit a wall, we found it
@@ -31,7 +32,8 @@ public class Enemy : MonoBehaviour
     		//Now check left middle right
     		bool hit = false;
     		for (int i = -1; i < 2; i++){
-    			Vector3 dir = playerSide * i + toPlayer;
+    			Vector3 dir = playerSide * i * 0.5f + toPlayer;
+                //Debug.Log(dir);
     			if(!Physics.Raycast(this.transform.position, dir.normalized, dir.magnitude, wallMask) && 
     				Physics.Raycast(this.transform.position, dir.normalized, dir.magnitude, playerMask)){
     				hit = true;
